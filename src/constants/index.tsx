49e30e9c804420dc6224ab889ddd3a11b20abeb8
garden11 <emailuseuse@gmail.com>
2023-03-06 16:24:25 +0900

    fix : 누락된 값 추가

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -5,7 +5,11 @@ export const AUTHORIZATION_HEADER_KEY = "x-auth-token";
 export const ENGLISH_LOWERCASE_NUMBER_REGEX = /^[a-z0-9_-]/;
 export const IMAGE_EXTENTION_REGEX = /(\.png|\.jpg|\.jpeg)$/i;
 
+// uris
 export const DEFAULT_PROFILE_IMAGE_URI =
   "/assets/images/default-profile-picture.png";
 export const DEFAULT_POST_THUMBNAIL_IMAGE_URI =
   "/assets/images/default-post-thumbnail.png";
+
+// page
+export const PAGE_NUMBER_BUTTON_LENGTH = 5;
