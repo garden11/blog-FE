d895b7105ab5a387bff599cf805ddd6ba5305cce
garden11 <emailuseuse@gmail.com>
2024-03-17 23:57:15 +0900

    refact : PostList 컴포넌트 key 추가

--- src/components/system-design/post/post-board/post-list/index.tsx
+++ src/components/system-design/post/post-board/post-list/index.tsx
@@ -1,16 +1,17 @@
 import _ from "lodash";
+import React from "react";
 
 // components
 import Stack from "src/components/design-system/stack";
 import Spacing from "src/components/design-system/spacing";
 import Line from "src/components/design-system/line";
+import Item from "./item";
 
 // styles
 import { spacing } from "src/styles/spacing";
 
 // types
 import { PostDetail } from "src/types/post";
-import Item from "./item";
 
 type Props = {
   list: PostDetail[];
@@ -22,7 +23,7 @@ const PostList = (props: Props) => {
     <Stack.Vertical>
       {props.list.map((listItem, index) => {
         return (
-          <>
+          <React.Fragment key={listItem.id}>
             <Item
               post={listItem}
               onClick={() => props.onClickListItem(listItem.id)}
@@ -34,7 +35,7 @@ const PostList = (props: Props) => {
                 <Spacing.Vertical size={spacing.unit30} />
               </>
             )}
-          </>
+          </React.Fragment>
         );
       })}
     </Stack.Vertical>
