e81e1bb162b521fdf2ca0ac3f5f98bf2fa67530b
garden11 <emailuseuse@gmail.com>
2024-04-29 01:22:57 +0900

    refact : 컴포넌트 분리

---
+++ src/components/design-system/pagination/prev/index.tsx
@@ -0,0 +1,24 @@
+import { cx } from "@emotion/css";
+
+type Props = {
+  disabled: boolean;
+  onClick: () => void;
+};
+
+const Prev = (props: Props) => {
+  return (
+    <li
+      className={cx("button", {
+        disabled: props.disabled,
+      })}
+      onClick={() => {
+        !props.disabled && props.onClick();
+      }}
+    >
+      {/** < */}
+      &lt;
+    </li>
+  );
+};
+
+export default Prev;
